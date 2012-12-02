ba192e8217a1675001d65621d202dfa846e6b520
Mangala SSS Khalsa <mangala@khalsa.com>
2012-12-01 17:02:26 -0700

    add basic init logic

--- app/main.js
+++ app/main.js
@@ -1,26 +1,81 @@
-//load the AMD modules we need
-require(['frozen/GameCore'], function(GameCore){
+require([
+	'dojo/on',
+	'frozen/GameCore'
+], function(on, GameCore){
+	// game state
+	var wall = [];
+	var bullets = [];
 
-  // game state
-  var x = 0;
-  var y = 0;
+	var phraseBox = document.getElementById('phraseBox');
 
-  //setup a GameCore instance
-  var game = new GameCore({
-    canvasId: 'game',
-    update: function(millis){
-      x += 1;
-      y += 1;
-    },
-    draw: function(context){
-      context.clearRect(0, 0, this.width, this.height);
-      context.fillRect(x, y, 50, 50);
-    }
-  });
+	function handleKeys(keyEvent){
+		// check if correct letters were typed
+		// if so, build up wall
+		var letter;
+		//console.dir(keyEvent)
+		if(keyEvent.keyCode > 64 && keyEvent.keyCode < 91){
+		 	letter = keyEvent.shiftKey ? String.fromCharCode(keyEvent.keyCode) : String.fromCharCode(keyEvent.keyCode + 32);
+		}
+		else{
+			letter = String.fromCharCode(keyEvent.keyCode);
+		}
+		//console.log(letter);
+	}
 
-  //if you want to take a look at the game object in dev tools
-  console.log(game);
+	on(document.body, 'keydown', handleKeys);
 
-  //launch the game!
-  game.run();
+	function checkCollision(){
+		// check for collision with wall
+		// check for collision with player
+	}
+
+	function moveBullets(){
+		checkCollision();
+	}
+
+	function setPhrase(){
+		phraseBox.innerHTML = getNextKey();
+	}
+	function getNextKey(){
+		return String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1)) + 65);
+	}
+
+	function drawWall(){
+		
+	}
+
+	function drawBullets(){
+		
+	}
+
+	//setup a GameCore instance
+	var game = new GameCore({
+		canvasId: 'game',
+
+		init: function(){
+			this.inherited(arguments);
+
+			setPhrase();
+		},
+
+		update: function(millis){
+			moveBullets();
+		},
+
+		draw: function(context){
+			drawWall();
+			drawBullets();
+			//context.clearRect(0, 0, this.width, this.height);
+			//context.fillRect(x, y, 50, 50);
+		}
+	});
+
+	//if you want to take a look at the game object in dev tools
+	console.log(game);
+
+	//launch the game!
+	on(document.getElementById('startGameButton'), 'click', function(event){
+		event.target.parentNode.removeChild(event.target);
+		game.run();
+	});
 });
