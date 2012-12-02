6ecfe44fb8e6cc845755f3e98f4f56253a9ee91f
Mangala SSS Khalsa <mangala@khalsa.com>
2012-12-01 20:20:00 -0700

    derp

--- app/main.js
+++ app/main.js
@@ -13,20 +13,40 @@ require([
 	var BULLET_POS_VERT_MIN;
 	var BULLET_SPEED_MIN = 1;
 	var BULLET_SPEED_MAX = 4;
+	
 
 	var game;
 
 	// game state
 	var wall = [];
 	var bullets = [];
+	var currentChar;
 
 	var phraseBox = document.getElementById('phraseBox');
+	var wallNodes = [];
+
 	var resourceManager = new ResourceManager();
 	var castleImage = resourceManager.loadImage('resources/images/castle.png');
 	var bulletImage = resourceManager.loadImage('resources/images/bullet.png');
 
+	var i;
+	var j;
+
+	for(i = 3; i > 0; i--){
+		for(j = 1; j < 6; j++){
+			wallNodes.push(document.getElementById('w' + i + 'r' + j));
+		}
+	}
+	console.dir(wallNodes)
+	for(i = 0; i < 15; i++){
+		wall.push(i);
+	}
 	function randomInt(lower, upper){
 		return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+	}
+
+	function createWalls(){
+		
 	}
 
 	function handleKeys(keyEvent){
@@ -34,22 +54,54 @@ require([
 		// if so, build up wall
 		var letter;
 		//console.dir(keyEvent)
-		if(keyEvent.keyCode > 64 && keyEvent.keyCode < 91){
-		 	letter = keyEvent.shiftKey ? String.fromCharCode(keyEvent.keyCode) : String.fromCharCode(keyEvent.keyCode + 32);
-		}
-		else{
-			letter = String.fromCharCode(keyEvent.keyCode);
-		}
+		//if(keyEvent.keyCode > 64 && keyEvent.keyCode < 91){
+		 	//letter = keyEvent.shiftKey ? String.fromCharCode(keyEvent.keyCode) : String.fromCharCode(keyEvent.keyCode + 32);
+		//}
+		//else{
+			//letter = String.fromCharCode(keyEvent.keyCode);
+		//}
 		//console.log(letter);
+
+		if(currentChar.charCodeAt(0) === keyEvent.keyCode){
+			addCharToWall(currentChar);
+			setPhrase();
+		}
+	}
+	function addCharToWall(char){
+		wallNodes[wall.pop()].innerHTML = char;
 	}
 
 	on(document.body, 'keydown', handleKeys);
+
+	var collisionMatrix = [];
+	for(i = 0; i < 3; i++){
+		for(j = 0; j < 5; j++){
+			if(!collisionMatrix[i]){
+				collisionMatrix[i] = [];
+			}
+			collisionMatrix[i][j] = {
+				x1: 500 + (50 * i),
+				x2: 550 + (50 * i),
+				y1: 170 + (70 * j),
+				y2: 240 + (70 * j)
+			};
+		}
+	}
 
 	function checkCollision(bullet){
 		// check for collision with wall
 		// check for collision with player
 		if(bullet[0] < CASTLE_WIDTH){
 			game.isRunning = false;
+		}
+		var xpos = bullet[0];
+		var ypos = bullet[1];
+		if(collisionMatrix.some(function(column){
+			return column.some(function(cell){
+				return xpos >= cell.x1 && xpos <= cell.x2 && ypos >= cell.y1 && ypos <= cell.y2;
+			});
+		})){
+			bullet[2] = 0;
 		}
 	}
 
@@ -61,11 +113,14 @@ require([
 			bullets.push(newBullet());
 		}
 		else{
+			bullets = bullets.filter(function(bullet){
+				return bullet[2] > 0;
+			})
 			bullets.forEach(function(bullet){
 				bullet[0] -= bullet[2];
 				checkCollision(bullet);
 			})
-			if(Math.random() < 0.03){
+			if(Math.random() < 0.01){
 				bullets.push(newBullet());
 			}
 		}
@@ -73,7 +128,7 @@ require([
 	}
 
 	function setPhrase(){
-		phraseBox.innerHTML = getNextKey();
+		phraseBox.innerHTML = currentChar = getNextKey();
 	}
 	function getNextKey(){
 		return String.fromCharCode(randomInt(65, 90));
@@ -101,6 +156,7 @@ require([
 			BULLET_POS_VERT_MAX = CANVAS_HEIGHT - CASTLE_HEIGHT;
 			BULLET_POS_VERT_MIN = CANVAS_HEIGHT - BULLET_HEIGHT
 
+			createWalls();
 			setPhrase();
 		},
 
